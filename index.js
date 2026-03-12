31d525b29de916f12fb5add95fc30fcdc1633c8f
Mohammad Anas <ansarianas53665@gmail.com>
2026-03-12 23:37:56 +0530

    dotenv install

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: "./.env",
+})
+
+let myusername = process.env.name
+let dataBase = process.env.database
+
+console.log("value:", myusername)
+console.log("value:", dataBase)
+
 console.log("Start of backend project");
