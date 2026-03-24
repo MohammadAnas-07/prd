bf348a092762bccd4610bac8be85394f81cf2b1e
Mohammad Anas <ansarianas53665@gmail.com>
2026-03-24 22:33:49 +0530

    All routes and validators of auth

--- src/validators/index.js
+++ src/validators/index.js
@@ -39,5 +39,37 @@ const userLoginValidator = () => {
 
 }
 
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword")
+    .notEmpty()
+    .withMessage("Old password is required"),
+    body("newPassword")
+    .notEmpty()
+    .withMessage("New password is required"),
+  ]
+}
 
-export {userRegisterValidator, userLoginValidator}
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail("Email is invalid")
+  ]
+}
+
+const userResetForgotPasswordValidator = () => {
+  return [
+    body("newPassword")
+     .notEmpty()
+     .withMessage("Password is required")
+  ]
+}
+
+
+export {
+  userRegisterValidator,
+   userLoginValidator,
+    userChangeCurrentPasswordValidator, userForgotPasswordValidator, userResetForgotPasswordValidator
+  }
