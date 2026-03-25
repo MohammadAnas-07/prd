0e7f8f8b2878e57130422175d4d3922831e6aae0
Mohammad Anas <ansarianas53665@gmail.com>
2026-03-25 20:24:58 +0530

    written project validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,5 @@
 import { body } from "express-validator"
+import { AvailableUserRole } from "../utils/constants.js"
 
 const userRegisterValidator = () => {
   return [
@@ -67,9 +68,37 @@ const userResetForgotPasswordValidator = () => {
   ]
 }
 
+const createValidator = () => {
+  return [
+    body("name")
+      .notEmpty()
+      .withMessage("Name is required"),
+    body("description")
+      .optional(),  
+  ]
+}
+
+const addMembertoProjectValidator = ()=>{
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("role")
+      .notEmpty()
+      .withMessage("Role is required")
+      .isIn(AvailableUserRole)    
+      .withMessage("Role is invalid")
+
+  ]
+}
 
 export {
   userRegisterValidator,
    userLoginValidator,
-    userChangeCurrentPasswordValidator, userForgotPasswordValidator, userResetForgotPasswordValidator
+    userChangeCurrentPasswordValidator, userForgotPasswordValidator, userResetForgotPasswordValidator,
+    createValidator,
+    addMembertoProjectValidator,
   }
